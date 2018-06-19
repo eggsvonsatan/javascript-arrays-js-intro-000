c04915ae3d5c0a37992c8a7a8c3e2b0929dce161
eggsvonsatan <elbadry.d@gmail.com>
2018-06-19 23:09:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,5 +6,6 @@ return chocolateBars};
 function addElementToEndOfArray (chocolateBars, hersheys) {return [...chocolateBars, hersheys]};
 function destructivelyAddElementToEndOfArray (chocolateBars, hersheys) {return chocolateBars.push(mars)};
 function accessElementInArray (array, index) {return array[index]};
-function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {chocolateBars.shift()};
-function removeElementFromBeginningOfArray (chocolateBars) {chocolateBars.slice(0)};
+function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {return chocolateBars.shift()};
+function removeElementFromBeginningOfArray (chocolateBars) {return chocolateBars.slice(0)};
+function destructivelyRemoveElementFromEndOfArray (chocolateBars) {return chocolateBars.pop()};
