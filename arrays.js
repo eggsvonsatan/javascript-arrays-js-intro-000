e4ea0060e32e52126d3810fc4c4ee75399624ecb
eggsvonsatan <elbadry.d@gmail.com>
2018-06-19 23:26:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,4 +9,4 @@ function accessElementInArray (array, index) {return array[index]};
 function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {chocolateBars.shift(); return chocolateBars};
 function removeElementFromBeginningOfArray (chocolateBars) {return chocolateBars.slice(1)};
 function destructivelyRemoveElementFromEndOfArray (chocolateBars) {chocolateBars.pop(); return chocolateBars};
-function removeElementFromEndOfArray (chocolateBars) {chocolateBars.slice(0, chocolateBars.length -1); return chocolateBars};
+function removeElementFromEndOfArray (chocolateBars) {return chocolateBars.slice(0, chocolateBars.length -1)};
