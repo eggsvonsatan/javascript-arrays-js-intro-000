adf62e41b3391c6b0dac869801d0c43138d95019
eggsvonsatan <elbadry.d@gmail.com>
2018-06-19 23:10:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,8 +3,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray (chocolateBars, mars) {return [mars, ...chocolateBars]};
 function destructivelyAddElementToBeginningOfArray (chocolateBars, mars) {chocolateBars.unshift(mars);
 return chocolateBars};
-function addElementToEndOfArray (chocolateBars, hersheys) {return [...chocolateBars, hersheys]};
-function destructivelyAddElementToEndOfArray (chocolateBars, hersheys) {return chocolateBars.push(mars)};
+function addElementToEndOfArray (chocolateBars, mars) {return [...chocolateBars, mars]};
+function destructivelyAddElementToEndOfArray (chocolateBars, mars) {return chocolateBars.push(mars)};
 function accessElementInArray (array, index) {return array[index]};
 function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {return chocolateBars.shift()};
 function removeElementFromBeginningOfArray (chocolateBars) {return chocolateBars.slice(0)};
