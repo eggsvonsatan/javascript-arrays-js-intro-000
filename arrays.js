08ef807a731fc73c777902c48361185efd5f3bd3
eggsvonsatan <elbadry.d@gmail.com>
2018-06-19 23:14:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ return chocolateBars};
 function addElementToEndOfArray (chocolateBars, mars) {return [...chocolateBars, mars]};
 function destructivelyAddElementToEndOfArray (chocolateBars, mars) {chocolateBars.push(mars); return chocolateBars};
 function accessElementInArray (array, index) {return array[index]};
-function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {chocolateBars.shift(), return chocolateBars};
+function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {chocolateBars.shift(); return chocolateBars};
 function removeElementFromBeginningOfArray (chocolateBars) {return chocolateBars.slice(0)};
 function destructivelyRemoveElementFromEndOfArray (chocolateBars) {return chocolateBars.pop()};
